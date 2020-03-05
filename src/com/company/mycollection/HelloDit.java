fba3dabb8f1667d51ed6fb63d39bb3116a4a8bfe
baqimeilaoban <baqimeilaoban@gmail.com>
2020-03-06 01:44:01 +0800

    最次提交1

--- src/com/company/mycollection/HelloDit.java
+++ src/com/company/mycollection/HelloDit.java
@@ -2,6 +2,6 @@ package com.company.mycollection;
 
 public class HelloDit {
     public static void main(String[] args) {
-        System.out.println("我是一个好人");
+        System.out.println("我是一个好人！！！！！");
     }
 }
